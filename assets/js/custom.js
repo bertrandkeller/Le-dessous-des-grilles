0c0f04eaaf0d61539954a2e18e3cfed5b06fe571
bertrand keller <bertrand.keller@gmail.com>
2013-10-11 13:54:02 +0200

    update prez

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,22 @@
 //custom js goes here
+$(function () {	
+	$('.grid-button-mod button').on('click',function(){
+		var $this = $(this) ;
+				classname = $this.attr('class');
+				classtype = classname.substring(9);
+		$('#grid-row').removeClass().addClass(classname);
+		$('h1 span').text(classtype);
+	});
+	$('.grid-dom-add').on('click',function(){
+		$( "#grid-row" ).append("<div class='grid-block'><div class='content'></div></div>");
+	});
+	$('.grid-dom-height').on('click',function(){
+		$( ".grid-block:nth-child(4n+4) .content" ).addClass("content-special");
+	});
+	$('.grid-dom-clear').on('click',function(){
+		$( ".grid-block:nth-child(6n+1)" ).addClass("clearleft");
+	});
+	$('.grid-dom-border').on('click',function(){
+		$( "#grid-row" ).toggleClass("grid--border");
+	});
+});
